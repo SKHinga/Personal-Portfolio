1c8bfd92cd8992741c21159843382752c914bb1c
SKHinga <samuelhinga200@gmail.com>
2022-02-28 19:20:52 +0300

    Script: script the text area

--- JS/main.js
+++ JS/main.js
@@ -2,3 +2,9 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+const textarea = document.querySelector("textarea");
+textarea.addEventListener("keyup", e =>{
+  let scHeight = e.target.scrollHeight;
+  textarea.style.height = `${scHeight}px`;
+})
